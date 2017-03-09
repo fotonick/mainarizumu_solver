11b9347637e4df786b9c9900012003860408fd9c
Nickolas Fotopoulos <nickolas.fotopoulos@synaptics.com>
2017-03-09 15:08:07 -0800

    Remove println in parser

--- src/csv_board_parser.rs
+++ src/csv_board_parser.rs
@@ -33,14 +33,12 @@ pub fn parse_csv_board<T : BufRead>(s : T) -> Option<(Board, BoardConstraints)> 
         // Even rows are either numbers or horizontal constraints.
         {
             let row_string = lines_iter.next().expect("can't read line").expect("can't read line 2");
-            println!("{}", row_string);
             let mut row_field_iter = row_string.split(',');
             for col in 0..(n - 1) {
                 let offset = row_offset + col;
                 let row_field = row_field_iter.next().expect("ran out of columns while looking for number");
                 board.allowed[offset] = if row_field == "" { unconstrained_digit } else { u64::from_str(&row_field).expect("could not parse number constraint") };
                 let row_field_2 = row_field_iter.next().expect("ran out of columns while looking for row constraint");
-                println!("About to make horizontal constraint");
                 board_constraints.horizontal[offset] = match_horizontal_constraint(row_field_2);
             }
             let row_field = row_field_iter.next().expect("ran out of columns while looking for number");
@@ -49,28 +47,22 @@ pub fn parse_csv_board<T : BufRead>(s : T) -> Option<(Board, BoardConstraints)> 
         // Odd rows are vertical constraints only.
         {
             let row_string = lines_iter.next().expect("can't read line").expect("can't read line 2");
-            println!("{}", row_string);
             let mut row_field_iter = row_string.split(',');
             for col in 0..(n-1) {
                 let offset = row_offset + col;
                 let row_field = row_field_iter.next().expect("ran out of columns while looking for vertical constraint");
-                println!("About to make vertical constraint");
                 board_constraints.vertical[offset] = match_vertical_constraint(row_field);
-                println!("made vertical constraint");
                 let row_field_2 = row_field_iter.next().expect("ran out of columns while looking for row constraint");
                 assert_eq!(row_field_2, "");
             }
             let row_field = row_field_iter.next().expect("ran out of columns while looking for vertical constraint");
-            println!("About to make vertical constraint");
             board_constraints.vertical[row_offset + n - 1] = match_vertical_constraint(row_field);
-            println!("made vertical constraint");
         }
     }
     let row_offset = n * (n - 1);
     // Even rows are either numbers or horizontal constraints.
     {
         let row_string = lines_iter.next().expect("can't read line").expect("can't read line 2");
-        println!("{}", row_string);
         let mut row_field_iter = row_string.split(',');
         for col in 0..(n - 1) {
             let offset = row_offset + col;
